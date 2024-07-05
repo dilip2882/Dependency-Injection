366bb3b086ff37971d71d5cb71824363b7001b7a
Dilip Patel <pateldilip2902@gmail.com>
2024-07-05 15:38:21 +0530

    daggerexample

--- DaggerExample/app/src/main/java/com/dilip/daggerexample/NotificationServiceModule.kt
+++ DaggerExample/app/src/main/java/com/dilip/daggerexample/NotificationServiceModule.kt
@@ -2,12 +2,20 @@ package com.dilip.daggerexample
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
 
+    @MessageQualifier
     @Provides
     fun getMessageService() : NotificationService {
         return MessageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService() : NotificationService {
+        return EmailService()
     }
 }
